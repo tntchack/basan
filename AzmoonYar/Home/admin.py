3a9c32fff6146af33efa671aec0e129058678c5c
basan <admin@basan.com>
2016-08-04 07:59:55 +0430

    مدل message به ادمین اضافه شد

--- AzmoonYar/Home/admin.py
+++ AzmoonYar/Home/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Message
 
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'date_of_birth', 'photo']
 
 
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created', 'status', 'type', 'publish']
+
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Message, MessageAdmin)
